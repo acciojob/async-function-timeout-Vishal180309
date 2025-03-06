edb9e0df2c536bc42e1a3ccfe9a67d34eb13af2a
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-06 10:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const form = document.getElementById("delay-form");
+const output = document.getElementById("output");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const text = document.getElementById("text").value;
+    const delay = parseInt(document.getElementById("delay").value);
+
+    await delayMessage(text, delay);
+});
+
+function delayMessage(text, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            output.innerText = text;
+            resolve();
+        }, delay);
+    });
+}
